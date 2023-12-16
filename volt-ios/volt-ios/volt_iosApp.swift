17b1edf5f7510ece2efde974318a3120396c428d
Matthew Rockey <matthew.rockey@crowdstreet.com>
2023-12-16 12:57:59 -0600

    init commit

---
+++ volt-ios/volt-ios/volt_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  volt_iosApp.swift
+//  volt-ios
+//
+//  Created by Matt Rockey on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct volt_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
